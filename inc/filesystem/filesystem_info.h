2eda43525d2c5c1f4c0c90ab7ebc3629aa2787b0
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-12-06 00:12:49 +0200

    Added PVS-Studio headers

--- inc/filesystem/filesystem_info.h
+++ inc/filesystem/filesystem_info.h
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 //
 // Created by andrew on 11/29/23.
 //
@@ -17,15 +19,15 @@
 struct FSInfo {
     FilesystemDump dump;
     Superblock super_block;
-    block_size_t block_size;
     DescriptorTable descriptor_table;
     FilesystemType type = FilesystemType::EXT2;
+    block_size_t block_size;
 
     explicit FSInfo(const FilesystemDump &dump) : dump(dump),
                                                   super_block(dump),
-                                                  block_size(super_block.get_block_size()),
                                                   descriptor_table(dump, super_block),
-                                                  type(get_fs_type(super_block)) {
+                                                  type(get_fs_type(super_block)),
+                                                  block_size(super_block.get_block_size()) {
         std::cout << "Filesystem type: " << get_fs_type_name(type) << "\n" << std::endl;
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "filesystem/filesystem_reader.h"
 #include "cl_options/options_parser.h"
 #include "errors/exception.h"
